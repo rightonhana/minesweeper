4dae02a920667b43d6b932563652ca990f9cf86d
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-07 18:57:27 -0700

    :sparkles: add level data constants

---
+++ src/constants/easy.ts
@@ -0,0 +1,7 @@
+import LevelData from "../types/LevelData";
+
+export const EASY: LevelData = {
+	bombs: 10,
+	height: 9,
+	width: 9
+}
---
+++ src/constants/medium.ts
@@ -0,0 +1,7 @@
+import LevelData from "../types/LevelData";
+
+export const MEDIUM: LevelData = {
+	bombs: 40,
+	height: 16,
+	width: 16
+};
