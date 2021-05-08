f9ded77ef0f0c243addf321effdccad5a031452a
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-08 10:10:36 -0700

    :sparkles: add revealBombs util

---
+++ src/utils/bombs/revealBombs.ts
@@ -0,0 +1,10 @@
+import MinesweeperState from "../../types/MinesweeperState";
+
+/**
+ * Reveal all level bombs
+ * @param state Minesweeper state matrix
+ */
+export const revealBombs = (state: MinesweeperState[][]) => 
+	state.map((row: MinesweeperState[]) =>
+		row.map((column: MinesweeperState) =>
+			column.bomb ? { ...column, discovered: true } : column));
