c4819fd1df8569419a022db3a3310c79a4d30b65
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-07 20:27:35 -0700

    :sparkles: add setCellValue util

---
+++ src/utils/setCellValue.ts
@@ -0,0 +1,24 @@
+import MinesweeperState from "../types/MinesweeperState";
+import { Tuple } from "../types/Tuple";
+import validXY from "./validXY";
+
+/**
+ * Set a Cell value based on a given [x, y]
+ * @param state Minesweeper state matrix
+ * @param param1 Position get the valid neighbors positions
+ * @param value value to update
+ */
+export const setCellValue = (
+	state: MinesweeperState[][],
+	[x, y]: Tuple<number>,
+	value: MinesweeperState
+) =>
+	validXY(state, [x, y])
+		? [
+			...state.slice(0, x),
+			[...state[x].slice(0, y), value, ...state[x].slice(y + 1)],
+			...state.slice(x + 1),
+		]
+		: state;
+
+export default setCellValue;
