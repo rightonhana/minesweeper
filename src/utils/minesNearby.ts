d4af974078b7c665f15f23d8cbe075562b876c34
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-10 10:34:18 -0700

    :sparkles: add minesNearby util

---
+++ src/utils/minesNearby.ts
@@ -0,0 +1,14 @@
+import MinesweeperState from "../types/MinesweeperState";
+import Tuple from "../types/Tuple";
+import validPositions from "./validPositions";
+
+/**
+ * Checks if there are mines nearby
+ * @param state Minesweeper state matrix
+ * @param param1 Position to check
+ */
+export const minesNearby = (state: MinesweeperState[][], [x, y]: Tuple<number>) => 
+	validPositions(state, [x, y]).some(([currentX, currentY]) => state[currentX][currentY].bomb);
+
+
+export default minesNearby;
