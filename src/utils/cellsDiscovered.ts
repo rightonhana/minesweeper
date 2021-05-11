63497503edc9d96b3e32d924e03d03ed4298f48b
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-11 14:05:58 -0700

    :sparkles: add cellsDiscovered util

---
+++ src/utils/cellsDiscovered.ts
@@ -0,0 +1,13 @@
+import MinesweeperState from "../types/MinesweeperState";
+
+/**
+ * Total number of cells discovered
+ * @param state Minesweeper state matrix
+ */
+export const cellsDiscovered = (state: MinesweeperState[][]) => state.reduce((total, rows) => {
+	return rows.reduce((acc, cell) => {
+		return cell.discovered && !cell.bomb ? acc + 1 : acc;
+	},0) + total;
+}, 0);
+
+export default cellsDiscovered;
