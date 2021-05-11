5a99f2c9513a8fb637c4969843f6549104bdb157
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-11 10:24:43 -0700

    :sparkles: add discoverEmptyZone util

---
+++ src/utils/discoverEmptyZone.ts
@@ -0,0 +1,21 @@
+import MinesweeperState from "../types/MinesweeperState";
+import Tuple from "../types/Tuple";
+import minesNearby from "./minesNearby";
+import setDiscovered from "./setDiscovered";
+import validPositions from "./validPositions";
+
+/**
+ * Discover an empty zone
+ * @param state Minesweeper state matrix
+ * @param param1 Position clicked
+ */
+export const discoverEmptyZone = (state:MinesweeperState[][], [x,y]:Tuple<number>): MinesweeperState[][] => 
+	!state[x][y].bomb && !minesNearby(state, [x,y])
+		? validPositions(state, [x, y]).reduce((newState: MinesweeperState[][], [validPositionX, validPositionY]: Tuple<number>)=> {
+			return !newState[validPositionX][validPositionY].flag
+				? setDiscovered(newState, [validPositionX, validPositionY])
+				: newState;
+			}, state)
+		: state;
+
+export default discoverEmptyZone;
