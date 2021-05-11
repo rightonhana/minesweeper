568a9bcc80aed0cdf02b33797ef60ef497e461a1
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-11 09:49:28 -0700

    :sparkles: add validPositions util

---
+++ src/utils/validPositions.ts
@@ -0,0 +1,29 @@
+import MinesweeperState from "../types/MinesweeperState";
+import { Tuple } from "../types/Tuple";
+import validXY from "./validXY";
+
+/**
+ * Return the valid neighbors positions of a given [x, y]
+ * @param state Minesweeper state matrix
+ * @param param1 Position get the valid neighbors positions
+ */
+export const validPositions = (
+	state: MinesweeperState[][],
+	[x, y]: Tuple<number>
+) => {
+	return ([
+		[x - 1, y - 1],
+		[x - 1, y],
+		[x - 1, y + 1],
+		[x, y - 1],
+		[x, y],
+		[x, y + 1],
+		[x + 1, y - 1],
+		[x + 1, y],
+		[x + 1, y + 1],
+	] as Tuple<number>[]).reduce((validPositions, currentValue) => {
+		return validXY(state, currentValue) ? [...validPositions, currentValue] : validPositions;
+	}, [] as Tuple<number>[]);
+};
+
+export default validPositions;
