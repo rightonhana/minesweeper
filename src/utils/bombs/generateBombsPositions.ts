a67a0c44a9d94f6ae4d23801f6d0b95fdac0d938
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-22 12:19:18 -0700

    :truck: rename loop attribute array to positions

--- src/utils/bombs/generateBombsPositions.ts
+++ src/utils/bombs/generateBombsPositions.ts
@@ -16,18 +16,18 @@ export const generateBombsPositions = (
 ): Tuple<number>[] => {
 	/**
 	 * Takes an array of bombs and returns a random x and y avoiding duplication.
-	 * @param array [x, y] positions
+	 * @param positions [x, y] positions
 	 * @param param1 [x, y] position
 	 * @param index Index value
 	 */
-	const loop = (array: Tuple<number>[], [x, y]: Tuple<number>, index = 0): Tuple<number>[] => {
+	const loop = (positions: Tuple<number>[], [x, y]: Tuple<number>, index = 0): Tuple<number>[] => {
 		const newArray = [
-		...array.slice(0, index),
-		randomNewPosition(array, cols, rows, [x, y]),
-		...array.slice(index + 1),
+		...positions.slice(0, index),
+		randomNewPosition(positions, cols, rows, [x, y]),
+		...positions.slice(index + 1),
 		];
 
-		return index + 1 === array.length ? newArray : loop(newArray, [x, y], index + 1);
+		return index + 1 === positions.length ? newArray : loop(newArray, [x, y], index + 1);
 	};
 
 	return loop([...new Array(bombs)], [x, y]);
