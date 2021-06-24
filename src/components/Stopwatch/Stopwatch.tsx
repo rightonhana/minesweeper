869414b52f7936fe4d3470fe1ab38dfafbc63e45
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-23 22:15:26 -0700

    :hammer: refactor Stopwatch component

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -1,17 +1,12 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
 import { FC } from "react";
 import formatTime from "../../utils/formatTime";
 import styles from "./Stopwatch.module.css";
 import StopwatchProps from "./StopwatchProps";
 
 export const Stopwatch: FC<StopwatchProps> = ({ timer = 0, ...props}) => (
-	<Card className={styles.Stopwatch} {...props}>
-		<CardContent>
-			<Typography className={styles.Text}>
-				{formatTime(timer)}
-			</Typography>
-		</CardContent>
-	</Card>
+	<span className={styles.Stopwatch} {...props}>
+		{formatTime(timer)}
+	</span>
 );
 
 export default Stopwatch;
