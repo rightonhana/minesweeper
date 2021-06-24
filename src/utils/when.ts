8a215d04adfb24be71f8e3ad1a2b59db82d6ae57
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-23 22:14:51 -0700

    :fire: remove when util

--- src/utils/when.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export const when = (
-	condition: boolean,
-	childrenWhenTrue: ReactNode,
-	childrenWhenFalse: ReactNode = null
-) => (condition ? childrenWhenTrue : childrenWhenFalse);
-
-export default when;
