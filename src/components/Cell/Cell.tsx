eb43889e4ba506c0f0cae6b6c866ebfce186c4d2
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-24 13:38:01 -0700

    :lipstick: update Cell styles

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -4,9 +4,6 @@ import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { FC } from "react";
 import CellProps from "./CellProps";
 
-/**TODO:
- * - Clear code
- */
 export const Cell: FC<CellProps> = ({ 
 	mine,
 	discovered,
@@ -15,40 +12,19 @@ export const Cell: FC<CellProps> = ({
 	...props
 }) => (
 	<button className={css`
-		color: ${
-			(discovered && !mine && value === 0)
-				? "var(--number0)"
-				: (discovered && !mine && value === 1)
-					? "var(--number1)"
-					: (discovered && !mine && value === 2)
-						? "var(--number2)"
-						: (discovered && !mine && value === 3)
-							? "var(--number3)"
-							: (discovered && !mine && value === 4)
-								? "var(--number4)"
-								: (discovered && !mine && value === 5)
-									? "var(--number5)"
-									: (discovered && !mine && value === 6)
-										? "var(--number6)"
-										: (discovered && !mine && value === 7)
-											? "var(--number7)"
-											: (discovered && !mine && value === 8)
-												? "var(--number8)"
-												: "var(--text)"
-		};
-
 		align-items: center;
-		border: 1rem solid ${ discovered ? "var(--cell-discovered)" : "var(--cell-undiscovered)"} transparent;
+		background-color: ${ discovered ? "var(--cell-discovered)" : "var(--primary-color)" };
 		border-radius: 10%;
+		border: 1rem solid ${ discovered ? "var(--cell-discovered)" : "var(--cell-undiscovered)" } transparent;
+		color: ${ (discovered && !mine) ? `var(--number${value})` : "var(--text)" };
 		display: flex;
 		font-size: 1rem;
 		font-weight: 900;
 		height: 2rem;
 		justify-content: center;
 		margin: 0;
-		width: 2rem;
-		background-color: ${discovered ? "var(--cell-discovered)" : "var(--primary-color)"};
 		user-select: none;
+		width: 2rem;
 		${discovered ? "pointer-events: none;" : "cursor: pointer;"}
 
 		:hover {
@@ -63,10 +39,9 @@ export const Cell: FC<CellProps> = ({
 	`} disabled={discovered} {...props}>
 		{ flag 
 			? <Flag style={{fill: discovered ? "var(--primary-color)" : "var(--text)"}}/>
-			: (!discovered
-				? ""
-				: (discovered && mine ? <HighlightOffIcon style={{fill: "black"}} /> : value)
-			)
+			: (discovered
+				? (mine ? <HighlightOffIcon style={{fill: "black"}} /> : value)
+				: "")
 		}
 	</button>
 );
